cd8480e16320414ac86bacea0b36b071737e05e8
augustinebest <augustusbest00@gmail.com>
2023-05-30 20:31:32 +0100

    Deleted CompanydetailsRoute

--- app/routes/companydetails.js
+++
@@ -1,8 +0,0 @@
-import Route from '@ember/routing/route';
-
-export default class CompanydetailsRoute extends Route {
-  model(params) {
-    const { comp_id } = params;
-    return comp_id;
-  }
-}
