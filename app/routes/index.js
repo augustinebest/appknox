b0885954fe7351b7ccb3d0d2593eacbfd0ba38bc
augustinebest <augustusbest00@gmail.com>
2023-05-30 20:32:31 +0100

    Added setupController & resetController function

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,31 +1,38 @@
-/* eslint-disable ember/no-get */
-/* eslint-disable ember/no-classic-classes */
 import Route from '@ember/routing/route';
-import { queryManager } from 'ember-apollo-client';
 import query from 'appknox/gql/queries/country';
-// import gql from 'graphql-tag';
+import { inject as service } from '@ember/service';
+import ApolloQueryManager from '../utils/apollo';
 
-// const query = gql`
-//   query countries {
-//     countries {
-//       id
-//     }
-//   }
-// `;
+export default class IndexRoute extends Route {
+  @service apollo;
+  queryManager = new ApolloQueryManager(this.apollo);
 
-// console.log('heree', query);
+  queryParams = {
+    count: {
+      refreshModel: true,
+    },
+  };
 
-// export default class IndexRoute extends Route {}
-export default Route.extend({
-  apollo: queryManager(),
+  async model(params) {
+    const count = params.count;
+    const result = await this.queryManager.query({
+      query,
+      variables: { first: count },
+      operationName: 'CountriesQuery',
+    });
+    return result.countries;
+  }
 
-  async model() {
-    // const variables = { id: params.id };
-    // const result = this.apollo.watchQuery({ query, variables }, 'countries');
-    // console.log('resultxx', result);
-    // return result;
-    const res = await this.get('apollo').query({ query }, 'countries');
-    console.log('resultssss', res);
-    return res;
-  },
-});
+  setupController(controller, model) {
+    super.setupController(...arguments);
+    controller.countries = model.edges;
+    controller.pageInfo = model.pageInfo;
+  }
+
+  // cleanup
+  resetController(controller, isExiting) {
+    if (isExiting) {
+      controller.send('resetController');
+    }
+  }
+}
